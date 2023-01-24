0c9968989994154108c7effc313ae75b4e7380df
phomearadev <phomearadev@gmail.com>
2023-01-24 19:30:18 +0000

    Updated 23 Jan 19:30

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 // Development Project to Fly a Tello Drone from a phone app
+// Updated
+
 
 
 export default function App() {
